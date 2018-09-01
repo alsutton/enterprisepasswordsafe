1e990f15231a38430a22eca401bffec7390512c9
Al Sutton <alsutton@users.noreply.github.com>
2018-09-01 18:38:23 +0100

    More simplification refactoring

--- src/main/java/com/enterprisepasswordsafe/ui/web/servlets/EditHierarchy.java
+++ src/main/java/com/enterprisepasswordsafe/ui/web/servlets/EditHierarchy.java
@@ -22,6 +22,7 @@ import java.security.GeneralSecurityException;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -100,57 +101,24 @@ public final class EditHierarchy extends HttpServlet {
 			User user = SecurityUtils.getRemoteUser(request);
 			SecurityUtils.isAllowedAccess(accessApprover, user);
 
-			ServletUtils servletUtils = ServletUtils.getInstance();
-		    String nodeId = servletUtils.getNodeId(request);
-		    if(nodeId == null) {
-		    	nodeId = HierarchyNode.ROOT_NODE_ID;
-		    }
+		    HierarchyNode node = closestVisibleNodeForUser(request, user);
 
-		    HierarchyNodeDAO hnDAO = HierarchyNodeDAO.getInstance();
-		    HierarchyNode node = hnDAO.getById(nodeId);
-		    if(node == null) {
-		    	nodeId = HierarchyNode.ROOT_NODE_ID;
-		    	node = hnDAO.getById(nodeId);
-		    }
-
-		    if( node.getType() == HierarchyNode.USER_CONTAINER_NODE ) {
-		    	throw new ServletException("Access to that node is forbidden.");
-		    }
-
-		    HierarchyNodeAccessRuleDAO hnarDAO = HierarchyNodeAccessRuleDAO.getInstance();
-		    List<HierarchyNode> parentage = hnDAO.getParentage(node);
-		    if( !user.isAdministrator()
-		    &&	hnarDAO.getAccessibilityForUser(node, user) == HierarchyNodeAccessRuleDAO.ACCESIBILITY_DENIED) {
-		        for( HierarchyNode thisNode : parentage ) {
-		        	if(hnarDAO.getAccessibilityForUser(thisNode, user) == HierarchyNodeAccessRuleDAO.ACCESIBILITY_DENIED) {
-		        		break;
-		        	}
-		        	node = thisNode;
-		    	}
-		        servletUtils.generateErrorMessage(
-		    			request,
-		    			"You are not allowed access to the folder you requested. You have been diverted to a folder you can access."
-		    		);
-		    	parentage = hnDAO.getParentage(node);
-		    }
+		    ServletUtils servletUtils = ServletUtils.getInstance();
 		    servletUtils.setCurrentNodeId(request, node.getNodeId());
 
 		    String action = request.getParameter(BaseServlet.ACTION_PARAMETER);
 
 		    request.setAttribute("node", node);
-		    request.setAttribute(BaseServlet.NODE_PARENTAGE, parentage);
-
-		    HttpSession session = request.getSession(false);
 
 		    // Check for movement.
 		    String oldNodeId = servletUtils.getNodeId(request);
-		    if (oldNodeId != null && !nodeId.equals(oldNodeId)) {
+		    if (oldNodeId != null && !node.getNodeId().equals(oldNodeId)) {
 		        action = null;
 		    }
 
 		    // Check for an action
 		    if (action != null && action.length() > 0) {
-		        String nextPage = performAction(session, request, user, action, node);
+		        String nextPage = performAction(request, user, action, node);
 		        if (nextPage != null) {
 		        	response.sendRedirect(request.getContextPath()+nextPage);
 		        	return;
@@ -160,7 +128,6 @@ public final class EditHierarchy extends HttpServlet {
 
 		    String hideEmpty = ConfigurationDAO.getValue(ConfigurationOption.HIDE_EMPTY_FOLDERS);
 		    boolean includeEmpty = (hideEmpty.equals(Configuration.HIDE_EMPTY_FOLDERS_OFF));
-
 		    if	( user.isSubadministrator() ){
 		        String displayEdit = ConfigurationDAO.getValue(ConfigurationOption.EDIT_USER_MINIMUM_USER_LEVEL);
 		        if( displayEdit != null && 	displayEdit.equals("S") ) {
@@ -168,13 +135,59 @@ public final class EditHierarchy extends HttpServlet {
 		        }
 		    }
 
-		    HierarchyNodeChildren children = hnDAO.getChildrenValidForUser(node, user, includeEmpty, null, null);
+		    HierarchyNodeChildren children =
+                    HierarchyNodeDAO.getInstance().getChildrenValidForUser(node, user, includeEmpty, null, null);
 		    request.setAttribute(BaseServlet.NODE_CHILDREN, children);
-		    servletUtils.setCurrentNodeId(request, nodeId);
-    	} catch(SQLException | GeneralSecurityException | CloneNotSupportedException e) {
+		    servletUtils.setCurrentNodeId(request, node.getNodeId());
+    	} catch(SQLException | GeneralSecurityException e) {
     		throw new ServletException("There was a problem editing the hierarchy.", e);
     	}
         request.getRequestDispatcher("/subadmin/edit_subnodes.jsp").forward(request, response);
+    }
+
+    private HierarchyNode closestVisibleNodeForUser(final HttpServletRequest request, User user)
+            throws SQLException, ServletException, GeneralSecurityException {
+        HierarchyNodeDAO hnDAO = HierarchyNodeDAO.getInstance();
+        HierarchyNode node = getValidNode(request, hnDAO);
+        HierarchyNodeAccessRuleDAO hnarDAO = HierarchyNodeAccessRuleDAO.getInstance();
+        List<HierarchyNode> parentage = hnDAO.getParentage(node);
+        if( !user.isAdministrator()
+                &&	hnarDAO.getAccessibilityForUser(node, user) == HierarchyNodeAccessRuleDAO.ACCESIBILITY_DENIED) {
+            for( HierarchyNode thisNode : parentage ) {
+                if(hnarDAO.getAccessibilityForUser(thisNode, user) == HierarchyNodeAccessRuleDAO.ACCESIBILITY_DENIED) {
+                    break;
+                }
+                node = thisNode;
+            }
+            ServletUtils.getInstance().generateErrorMessage(request,
+                    "You are not allowed access to the folder you requested. You have been diverted to a folder you can access."
+            );
+            parentage = hnDAO.getParentage(node);
+        }
+        request.setAttribute(BaseServlet.NODE_PARENTAGE, parentage);
+        return node;
+    }
+
+    private HierarchyNode getValidNode(final HttpServletRequest request,
+                                       final HierarchyNodeDAO hnDAO)
+            throws ServletException, SQLException {
+        ServletUtils servletUtils = ServletUtils.getInstance();
+        String nodeId = servletUtils.getNodeId(request);
+        if(nodeId == null) {
+            nodeId = HierarchyNode.ROOT_NODE_ID;
+        }
+
+        HierarchyNode node = hnDAO.getById(nodeId);
+        if(node == null) {
+            nodeId = HierarchyNode.ROOT_NODE_ID;
+            node = hnDAO.getById(nodeId);
+        }
+
+        if( node.getType() == HierarchyNode.USER_CONTAINER_NODE ) {
+            throw new ServletException("Access to that node is forbidden.");
+        }
+
+        return node;
     }
 
     /**
@@ -187,140 +200,152 @@ public final class EditHierarchy extends HttpServlet {
     	doGet(request, response);
     }
 
-    private String performAction(final HttpSession session, final HttpServletRequest request, final User user,
-    		final String action, final HierarchyNode node)
-            throws SQLException, GeneralSecurityException, IOException, CloneNotSupportedException {
+    private String performAction(final HttpServletRequest request, final User user, final String action,
+                                 final HierarchyNode node)
+            throws SQLException, GeneralSecurityException, IOException {
         if (action.equals(EditHierarchy.ADD_ACTION)) {
             return "/subadmin/add_subnode.jsp";
         }
 
-        ServletUtils servletUtils = ServletUtils.getInstance();
         switch (action) {
             case EditHierarchy.COPY_ACTION:
             case EditHierarchy.DEEP_COPY_ACTION:
             case EditHierarchy.CUT_ACTION: {
-                String[] nodes = request
-                        .getParameterValues(BaseServlet.NODE_LIST_PARAMETER);
-                if (nodes == null || nodes.length == 0) {
-                    StringBuilder message = new StringBuilder();
-                    if (action.equals(EditHierarchy.COPY_ACTION) || action.equals(EditHierarchy.DEEP_COPY_ACTION)) {
-                        message.append("Copy");
-                    } else if (action.equals(EditHierarchy.CUT_ACTION)) {
-                        message.append("Cut");
-                    }
-                    message.append(" failed, No items were selected.");
-                    servletUtils.generateErrorMessage(request, message.toString());
-                    return null;
-                }
-
-                HierarchyNodeDAO hnDAO = HierarchyNodeDAO.getInstance();
-                for (int i = 0; i < nodes.length; i++) {
-                    String thisNode = nodes[i];
-                    if (thisNode.startsWith("p_")) {
-                        String passwordId = thisNode.substring(2);
-                        nodes[i] = hnDAO.getNodeIDForObject(node.getNodeId(), passwordId);
-                    }
-                }
-
-                session.setAttribute(BaseServlet.ACTION_PARAMETER, action);
-                session.setAttribute(BaseServlet.NODE_LIST_PARAMETER, nodes);
-
-                StringBuilder message = new StringBuilder();
-                message.append("The selected item");
-                if (nodes.length > 1) {
-                    message.append("s have");
-                } else {
-                    message.append(" has");
-                }
-                message.append(" been ");
-
-                if (action.equals(EditHierarchy.COPY_ACTION) || action.equals(EditHierarchy.DEEP_COPY_ACTION)) {
-                    message.append("copied.");
-                } else {
-                    message.append("cut.");
-                }
-
-                servletUtils.generateMessage(request, message.toString());
+                processSelection(request, action, node);
                 break;
             }
             case EditHierarchy.PASTE_ACTION: {
-                String sessonAction = (String) session
-                        .getAttribute(BaseServlet.ACTION_PARAMETER);
-                String[] nodes = (String[]) session
-                        .getAttribute(BaseServlet.NODE_LIST_PARAMETER);
-                if (sessonAction != null && nodes != null && nodes.length > 0) {
-                    HierarchyNodeDAO hnDAO = HierarchyNodeDAO.getInstance();
-                    switch (sessonAction) {
-                        case EditHierarchy.COPY_ACTION:
-                            pasteCopiedNodes(hnDAO, node, nodes);
-                            break;
-                        case EditHierarchy.DEEP_COPY_ACTION:
-                            pasteDeepCopiedNodes(hnDAO, node, nodes);
-                            break;
-                        case EditHierarchy.CUT_ACTION:
-                            pasteCutNodes(hnDAO, node, nodes);
-                            break;
-                        default:
-                            servletUtils.generateErrorMessage(request, "The items selected were not copied or pasted.");
-                            break;
-                    }
-                } else {
-                    servletUtils.generateErrorMessage(request, "Paste failed. No items have been cut or copied.");
-                }
-
-                session.removeAttribute(BaseServlet.ACTION_PARAMETER);
-                session.removeAttribute(BaseServlet.NODE_LIST_PARAMETER);
+                processPaste(request, node);
                 break;
             }
             case EditHierarchy.DELETE_ACTION: {
-                String[] nodes = request
-                        .getParameterValues(BaseServlet.NODE_LIST_PARAMETER);
-                if (nodes == null || nodes.length == 0) {
-                    servletUtils.generateErrorMessage(request, "Delete failed, No items were selected.");
-                    return null;
-                }
-
-                HierarchyNodeDAO hnDAO = HierarchyNodeDAO.getInstance();
-                // TODO : Check permissions of subnodes, and block if
-                // the user can not access a specific subnode.
-                for (String thisNodeId : nodes) {
-                    // Skip deletiong of any non-existant nodes
-                    if (thisNodeId != null && thisNodeId.startsWith("p_")) {
-                        thisNodeId = hnDAO.getNodeIDForObject(node.getNodeId(), thisNodeId.substring(2));
-                    }
-                    if (thisNodeId == null) {
-                        continue;
-                    }
-                    HierarchyNode thisNode = hnDAO.getById(thisNodeId);
-                    if (thisNode == null) {
-                        continue;
-                    }
-
-                    if (isNotDeletableByUser(user, thisNode)) {
-                        servletUtils.generateErrorMessage(request, "Delete failed, You do not have the permissions needed to delete all the objects specified.");
-                        return null;
-                    }
-                }
-
-                // Now the permissions have been checked we can perform the delete.
-                for (String thisNodeId : nodes) {
-                    if (thisNodeId != null && thisNodeId.startsWith("p_")) {
-                        thisNodeId = hnDAO.getNodeIDForObject(node.getNodeId(), thisNodeId.substring(2));
-                    }
-                    if (thisNodeId == null) {
-                        continue;
-                    }
-
-                    HierarchyNode thisNode = hnDAO.getById(thisNodeId);
-                    if (thisNode != null) {
-                        hnDAO.deleteNode(thisNode, user);
-                    }
-                }
+                processDelete(request, user, node);
                 break;
             }
         }
 
         return null;
+    }
+
+    private void processSelection(HttpServletRequest request, String action, final HierarchyNode node)
+            throws SQLException {
+        String[] nodes = request.getParameterValues(BaseServlet.NODE_LIST_PARAMETER);
+        if (nodes == null || nodes.length == 0) {
+            StringBuilder message = new StringBuilder();
+            if (action.equals(EditHierarchy.COPY_ACTION) || action.equals(EditHierarchy.DEEP_COPY_ACTION)) {
+                message.append("Copy");
+            } else if (action.equals(EditHierarchy.CUT_ACTION)) {
+                message.append("Cut");
+            }
+            message.append(" failed, No items were selected.");
+            ServletUtils.getInstance().generateErrorMessage(request, message.toString());
+            return;
+        }
+
+        HierarchyNodeDAO hnDAO = HierarchyNodeDAO.getInstance();
+        for (int i = 0; i < nodes.length; i++) {
+            String thisNode = nodes[i];
+            if (thisNode.startsWith("p_")) {
+                String passwordId = thisNode.substring(2);
+                nodes[i] = hnDAO.getNodeIDForObject(node.getNodeId(), passwordId);
+            }
+        }
+
+        HttpSession session = request.getSession();
+        session.setAttribute(BaseServlet.ACTION_PARAMETER, action);
+        session.setAttribute(BaseServlet.NODE_LIST_PARAMETER, nodes);
+
+        ServletUtils.getInstance().generateMessage(
+                request, generateOperationSuccessMessage(action, nodes.length > 1));
+    }
+
+    private String generateOperationSuccessMessage(String action, boolean isForMultiple) {
+        StringBuilder message = new StringBuilder();
+        message.append("The selected item");
+        message.append(isForMultiple ? "s have" : " has");
+        message.append(" been ");
+        if (action.equals(EditHierarchy.COPY_ACTION) || action.equals(EditHierarchy.DEEP_COPY_ACTION)) {
+            message.append("copied.");
+        } else {
+            message.append("cut.");
+        }
+        return message.toString();
+    }
+
+    private void processPaste(final HttpServletRequest request, final HierarchyNode node)
+            throws GeneralSecurityException, SQLException {
+        HttpSession session = request.getSession();
+        String sessonAction = (String) session.getAttribute(BaseServlet.ACTION_PARAMETER);
+        String[] nodes = (String[]) session.getAttribute(BaseServlet.NODE_LIST_PARAMETER);
+        if (sessonAction != null && nodes != null && nodes.length > 0) {
+            HierarchyNodeDAO hnDAO = HierarchyNodeDAO.getInstance();
+            switch (sessonAction) {
+                case EditHierarchy.COPY_ACTION:
+                    pasteCopiedNodes(hnDAO, node, nodes);
+                    break;
+                case EditHierarchy.DEEP_COPY_ACTION:
+                    pasteDeepCopiedNodes(hnDAO, node, nodes);
+                    break;
+                case EditHierarchy.CUT_ACTION:
+                    pasteCutNodes(hnDAO, node, nodes);
+                    break;
+                default:
+                    ServletUtils.getInstance().generateErrorMessage(request, "The items selected were not copied or pasted.");
+                    break;
+            }
+        } else {
+            ServletUtils.getInstance().generateErrorMessage(request, "Paste failed. No items have been cut or copied.");
+        }
+
+        session.removeAttribute(BaseServlet.ACTION_PARAMETER);
+        session.removeAttribute(BaseServlet.NODE_LIST_PARAMETER);
+    }
+
+    private void processDelete(final HttpServletRequest request, final User viewingUser, final HierarchyNode node)
+            throws SQLException, GeneralSecurityException, IOException {
+        String[] nodes = request.getParameterValues(BaseServlet.NODE_LIST_PARAMETER);
+        if (nodes == null || nodes.length == 0) {
+            ServletUtils.getInstance().generateErrorMessage(request, "Delete failed, No items were selected.");
+            return;
+        }
+
+        HierarchyNodeDAO hnDAO = HierarchyNodeDAO.getInstance();
+        // TODO : Check permissions of subnodes, and block if
+        // the user can not access a specific subnode.
+        for (String thisNodeId : nodes) {
+            // Skip deletiong of any non-existant nodes
+            if (thisNodeId != null && thisNodeId.startsWith("p_")) {
+                thisNodeId = hnDAO.getNodeIDForObject(node.getNodeId(), thisNodeId.substring(2));
+            }
+            if (thisNodeId == null) {
+                continue;
+            }
+            HierarchyNode thisNode = hnDAO.getById(thisNodeId);
+            if (thisNode == null) {
+                continue;
+            }
+
+            if (isNotDeletableByUser(viewingUser, thisNode)) {
+                ServletUtils.getInstance().generateErrorMessage(request,
+                        "Delete failed, You do not have the permissions needed to delete all the objects specified.");
+                return ;
+            }
+        }
+
+        // Now the permissions have been checked we can perform the delete.
+        for (String thisNodeId : nodes) {
+            if (thisNodeId != null && thisNodeId.startsWith("p_")) {
+                thisNodeId = hnDAO.getNodeIDForObject(node.getNodeId(), thisNodeId.substring(2));
+            }
+            if (thisNodeId == null) {
+                continue;
+            }
+
+            HierarchyNode thisNode = hnDAO.getById(thisNodeId);
+            if (thisNode != null) {
+                hnDAO.deleteNode(thisNode, viewingUser);
+            }
+        }
     }
 
     private boolean isNotDeletableByUser(final User user, final HierarchyNode node)
