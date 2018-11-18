42b5429adee192054ce8c3e6843a02661bbcf01c
Al Sutton <al.sutton@alsutton.com>
2018-11-18 10:25:14 +0000

    Extract commoon JDBC store methods into a base class

---
+++ src/main/java/com/enterprisepasswordsafe/engine/configuration/GenericJDBCConnectionInformation.java
@@ -0,0 +1,77 @@
+package com.enterprisepasswordsafe.engine.configuration;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.GeneralSecurityException;
+import java.util.prefs.Preferences;
+
+public class GenericJDBCConnectionInformation
+        implements JDBCConnectionInformation {
+
+    public String dbType;
+
+    public String driver;
+
+    public String url;
+
+    public String username;
+
+    public String password;
+
+    public GenericJDBCConnectionInformation() {
+        super();
+    }
+
+    GenericJDBCConnectionInformation(final String dbType, final String driver,
+            final String url, final String username, final String password) {
+        this.dbType = dbType;
+        this.driver = driver;
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public boolean isValid() {
+        return dbType != null && driver != null && url != null && username != null && password != null;
+    }
+
+    @Override
+    public int hashCode() {
+        return dbType.hashCode() | driver.hashCode() | url.hashCode()
+                | username.hashCode() | password.hashCode();
+    }
+
+    public boolean equals(GenericJDBCConnectionInformation otherConfig) {
+        return otherConfig != null && (dbType.equals(otherConfig.dbType)
+            && driver.equals(otherConfig.driver) && url.equals(otherConfig.url)
+            && username.equals(otherConfig.username) && password.equals(otherConfig.password));
+    }
+
+    @Override
+    public String toString() {
+        return  "DB Type: " + dbType + ", Driver: " + driver + ", URL: " + url +
+                ", Username: " + username + ", Password: " + password;
+    }
+
+    public String getDbType() {
+        return dbType;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
--- src/main/java/com/enterprisepasswordsafe/engine/configuration/PropertyBackedJDBCConnectionInformation.java
+++ src/main/java/com/enterprisepasswordsafe/engine/configuration/PropertyBackedJDBCConnectionInformation.java
@@ -7,7 +7,7 @@ import java.security.GeneralSecurityException;
 import java.util.prefs.Preferences;
 
 public class PropertyBackedJDBCConnectionInformation
-        implements JDBCConnectionInformation {
+        extends GenericJDBCConnectionInformation {
     private static final byte[] PASSWORD_AES_KEY = { 84, -54, 102, -106, 30,
             -63, 98, 125, 52, 22, 34, 44, -39, 86, 11, -120 };
 
@@ -23,104 +23,39 @@ public class PropertyBackedJDBCConnectionInformation
 
     private static final String ENCRYPTED_PASSWORD_PARAMETER = "eps_jdbc.password.encrypted";
 
-    public String dbType;
-
-    public String driver;
-
-    public String url;
-
-    public String username;
-
-    public String password;
-
     public PropertyBackedJDBCConnectionInformation() {
         super();
     }
 
     PropertyBackedJDBCConnectionInformation(Preferences prefs) throws GeneralSecurityException {
-        dbType = prefs.get(DB_TYPE_PARAMETER, null);
-        driver = prefs.get(DRIVER_PARAMETER, null);
-        url = prefs.get(URL_PARAMETER, null);
-        username = prefs.get(USERNAME_PARAMETER, null);
+        super(
+                prefs.get(DB_TYPE_PARAMETER, null),
+                prefs.get(DRIVER_PARAMETER, null),
+                prefs.get(URL_PARAMETER, null),
+                prefs.get(USERNAME_PARAMETER, null),
+                decodePassword(prefs)
+        );
+    }
+
+    private static String decodePassword(Preferences prefs) throws GeneralSecurityException {
         byte[] passwordBytes = prefs.getByteArray(ENCRYPTED_PASSWORD_PARAMETER, null);
-        if (passwordBytes == null) {
-            password = prefs.get(PASSWORD_PARAMETER, null);
-            if (password != null) {
-                prefs.putByteArray(ENCRYPTED_PASSWORD_PARAMETER, encryptPasswordText(password));
-                prefs.remove(PASSWORD_PARAMETER);
-            }
-        } else {
-            password = decryptPasswordText(passwordBytes);
+        if(passwordBytes == null) {
+            return prefs.get(PASSWORD_PARAMETER, null);
         }
+        return decryptPasswordText(passwordBytes);
     }
 
-    void storeIn(Preferences prefs)
-            throws GeneralSecurityException {
-        prefs.put(DB_TYPE_PARAMETER, dbType);
-        prefs.put(DRIVER_PARAMETER, driver);
-        prefs.put(URL_PARAMETER, url);
-        prefs.put(USERNAME_PARAMETER, username);
-        prefs.putByteArray(ENCRYPTED_PASSWORD_PARAMETER, encryptPasswordText(password));
-    }
-
-    public boolean isValid() {
-        return dbType != null && driver != null && url != null && username != null && password != null;
-    }
-
-    @Override
-    public int hashCode() {
-        return dbType.hashCode() | driver.hashCode() | url.hashCode()
-                | username.hashCode() | password.hashCode();
-    }
-
-    public boolean equals(PropertyBackedJDBCConnectionInformation otherConfig) {
-        return otherConfig != null && (dbType.equals(otherConfig.dbType)
-            && driver.equals(otherConfig.driver) && url.equals(otherConfig.url)
-            && username.equals(otherConfig.username) && password.equals(otherConfig.password));
-    }
-
-    @Override
-    public String toString() {
-        return  "DB Type: " + dbType + ", Driver: " + driver + ", URL: " + url +
-                ", Username: " + username + ", Password: " + password;
-    }
-
-    private byte[] encryptPasswordText(String text)
-            throws GeneralSecurityException {
-        return feedThroughCipher(Cipher.ENCRYPT_MODE, text.getBytes());
-    }
-
-    private String decryptPasswordText(byte[] data)
+    private static String decryptPasswordText(byte[] data)
             throws GeneralSecurityException {
         byte[] original = feedThroughCipher(Cipher.DECRYPT_MODE, data);
         return new String(original);
     }
 
-    private byte[] feedThroughCipher(int mode, byte[] data)
+    private static byte[] feedThroughCipher(int mode, byte[] data)
             throws GeneralSecurityException {
         SecretKey cryptoKey = new SecretKeySpec(PASSWORD_AES_KEY, "AES");
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(mode, cryptoKey);
         return cipher.doFinal(data);
-    }
-
-    public String getDbType() {
-        return dbType;
-    }
-
-    public String getDriver() {
-        return driver;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
     }
 }
