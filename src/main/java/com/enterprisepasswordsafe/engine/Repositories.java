4635545dfd11219a859ea5995f93020f76eea7a7
Al Sutton <al.sutton@alsutton.com>
2018-11-18 11:43:39 +0000

    Switch to environment backed JDBC info by default

--- src/main/java/com/enterprisepasswordsafe/engine/Repositories.java
+++ src/main/java/com/enterprisepasswordsafe/engine/Repositories.java
@@ -1,12 +1,12 @@
 package com.enterprisepasswordsafe.engine;
 
+import com.enterprisepasswordsafe.engine.configuration.EnvironmentVariableBackedJDBCConfigurationRepository;
 import com.enterprisepasswordsafe.engine.configuration.JDBCConfigurationRepository;
-import com.enterprisepasswordsafe.engine.configuration.PropertyBackedJDBCConfigurationRepository;
 import com.enterprisepasswordsafe.engine.dbpool.DatabasePoolFactory;
 
 public class Repositories {
     public static JDBCConfigurationRepository jdbcConfigurationRepository
-            = new PropertyBackedJDBCConfigurationRepository();
+            = new EnvironmentVariableBackedJDBCConfigurationRepository();
 
     public static DatabasePoolFactory databasePoolFactory
             = new DatabasePoolFactory();
